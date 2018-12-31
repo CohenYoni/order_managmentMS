50261793ba280eed47445b72d4cc9493662dbd19
Yoni Cohen <jonbir3@gmail.com>
2019-01-01 01:41:31 +0200

    Yoni create the main script

---
+++ mainScript.py
@@ -0,0 +1,182 @@
+import tkinter as tk
+import os, subprocess, json, time, sqlite3
+
+DBname = 'ordersManagmentDB.db'
+pathToDB = os.getcwd() + '\\' + DBname
+orderTableName = 'orders'
+itemsOfOrdersTableName = 'itemsOfOrder'
+
+def showErrorDetails(err):
+    subprocess.Popen(['java', '-cp', 'java-json.jar;', 'ShowError', json.dumps({'error': str(err)})], shell=True)
+    print('running at background...')
+
+# override tkitner handling exceptoin function
+def custom_report_callback_exception(self, exc_type, exc_value, exc_traceback):
+    showErrorDetails(exc_value)
+tk.Tk.report_callback_exception = custom_report_callback_exception
+
+class MainPane():
+    #static variables
+    btnWidth = 40
+    
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Orders Management')
+        headerLbl = tk.Label(self.root, text="Welcome to Orders Management app",fg="blue")
+        headerLbl.grid(row=0)
+        createOrderBtn = tk.Button(self.root, text='Create New Order', width=MainPane.btnWidth, command=self.createNewOrder)
+        createOrderBtn.grid(row=1)
+        showOrdersBtn = tk.Button(self.root, text='Show All Orders', width=MainPane.btnWidth, command=self.showAllOrders)
+        showOrdersBtn.grid(row=2)
+        showIncomsBtn = tk.Button(self.root, text='Show Incomes From Orders', width=MainPane.btnWidth, command=self.showIncomesFromOrders)
+        showIncomsBtn.grid(row=3)
+        showInventoryBtn = tk.Button(self.root, text='Show Inventory', width=MainPane.btnWidth, command=self.showInventory)
+        showInventoryBtn.grid(row=4)
+
+    def start(self):
+        self.root.mainloop()
+
+    def close(self):
+        self.root.destroy()
+
+    def createNewOrder(self):
+        def addProducts():
+            def addProductToOrder():
+                prodID = prodEntry.get()
+                if prodID != '' and prodID.isnumeric() and int(prodID) in allProductsIDs:
+                    productIDs.append(int(prodID))
+                    prodEntry.delete(0, tk.END)
+                else:
+                    showErrorDetails('Wrong product ID!')
+                    
+            def finish():
+                if len(productIDs) != 0:
+                    date = time.strftime("%d/%m/%Y")
+                    connectDB = sqlite3.connect(pathToDB)
+                    cursorDB = connectDB.cursor()
+                    record = cursorDB.execute('SELECT MAX(ordID) FROM {0};'.format(orderTableName))
+                    lastOrdID = record.fetchone()[0]
+                    cursorDB.execute('INSERT INTO {0} (ordID, custName, custPhone, ordDate) VALUES ({1}, "{2}", "{3}", "{4}");'.format(orderTableName, lastOrdID + 1, name, phone, date))
+                    productsNames = []
+                    for prodID in productIDs:
+                        cursorDB.execute('INSERT INTO {0} (ordID, productID) VALUES ({1}, {2});'.format(itemsOfOrdersTableName, lastOrdID + 1, prodID))
+                        jsonToGetNameMS = {'DBlocation':pathToDB , 'prodID':prodID}
+                        nameJson = json.loads(subprocess.check_output(['python', 'getNameOfProductFromDB.py', json.dumps(jsonToGetNameMS)], shell=True))
+                        productsNames.append(nameJson['output']['prodName'])
+                    connectDB.commit()
+                    order = {
+                                'ordID':lastOrdID+1,
+                                'custName': name,
+                                'custPhone': phone,
+                                'ordDate': date,
+                                'productsIDs': productIDs
+                            }
+                    totalPriceJson = json.loads(subprocess.check_output(['python', 'MSsumTotalPrice.py', json.dumps({'DBlocation': pathToDB, 'order': order})], shell=True))
+                    if totalPriceJson['hadError']:
+                        showErrorDetails(totalPriceJson['error'])
+                    print(totalPriceJson)
+                    prodLbl.destroy()
+                    prodEntry.destroy()
+                    addProdBtn.destroy()
+                    finishBtn.destroy()
+                    tk.Label(createOrderWindow, text="The order saved successfully!").grid(row = 0)
+                    tk.Label(createOrderWindow, text="The total price of the order is {0}".format(totalPriceJson['output'])).grid(row = 1)
+                    order.pop('productsIDs', None)
+                    order['productsNames'] = productsNames
+                    subprocess.Popen(['java', '-cp', 'java-json.jar;', 'ShowOrdersDetailsInList', json.dumps({'orders': [order,]})], shell=True)
+                else:
+                    showErrorDetails('You must add at least one product!')
+                
+
+            name = nameEntry.get()
+            phone = phoneEntry.get()
+            if name != '' and phone != '':
+                nameLbl.destroy()
+                nameEntry.destroy()
+                phoneLbl.destroy()
+                phoneEntry.destroy()
+                addOrderBtn.destroy()
+                prodLbl = tk.Label(createOrderWindow, text="Enter the product ID here: ")
+                prodLbl.grid(row = 0)
+                prodEntry = tk.Entry(createOrderWindow, width = 20)
+                prodEntry.grid(row=0, column=1, columnspan=50)
+                productIDs = []
+                addProdBtn = tk.Button(createOrderWindow, text='add product', command=addProductToOrder)
+                addProdBtn.grid(row = 1)
+                finishBtn = tk.Button(createOrderWindow, text='finish', command=finish)
+                finishBtn.grid(row = 1, column = 1)
+                allProductsJson = json.loads(subprocess.check_output(['python', 'getAllProductsFromDB.py', pathToDB], shell=True))
+                if allProductsJson['hadError']:
+                    showErrorDetails(allProductsJson['error'])
+                allProductsIDs = tuple(map(lambda x: x['prodID'], allProductsJson['output']))
+                subprocess.Popen(['java', '-cp', 'java-json.jar;', 'ShowAllProductsInTable', json.dumps({'products': allProductsJson['output']})],shell=True)
+            else:
+                showErrorDetails('Please enter a name and phone!')
+            
+                
+        createOrderWindow = tk.Tk()
+        createOrderWindow.title('Create Order')
+        nameLbl = tk.Label(createOrderWindow, text="Full Name: ")
+        nameLbl.grid(row = 1)
+        nameEntry = tk.Entry(createOrderWindow, width = 50)
+        nameEntry.grid(row=1, column=1, columnspan=50)
+        phoneLbl = tk.Label(createOrderWindow, text="Phone number: ")
+        phoneLbl.grid(row = 2)
+        phoneEntry = tk.Entry(createOrderWindow, width = 50)
+        phoneEntry.grid(row=2, column=2, columnspan=50)
+        addOrderBtn = tk.Button(createOrderWindow, text='create', command=addProducts)
+        addOrderBtn.grid(row=3)
+
+    def showAllOrders(self):
+        allOrdersJson = json.loads(subprocess.check_output(['python', 'getAllOrdersFromDB.py', pathToDB], shell=True))
+        if allOrdersJson['hadError']:
+            showErrorDetails(allOrdersJson['error'])
+        for order in allOrdersJson['output']:
+            productsNames = []
+            for prodId in order['productsIDs']:
+                jsonToGetNameMS = {'DBlocation':pathToDB , 'prodID':prodId}
+                nameJson = json.loads(subprocess.check_output(['python', 'getNameOfProductFromDB.py', json.dumps(jsonToGetNameMS)], shell=True))
+                productsNames.append(nameJson['output']['prodName'])
+            order.pop('productsIDs', None) #remove the productIDs key from the dictionary
+            order['productsNames'] = productsNames
+        subprocess.Popen(['java', '-cp', 'java-json.jar;', 'ShowOrdersDetailsInTable', json.dumps({'orders': allOrdersJson['output']})],shell=True)
+        print('running at background...')
+
+    def showIncomesFromOrders(self):
+        allOrdersJson = json.loads(subprocess.check_output(['python', 'getAllOrdersFromDB.py', pathToDB], shell=True))
+        if allOrdersJson['hadError']:
+            showErrorDetails(allOrdersJson['error'])
+        totalIncomes = 0
+        for order in allOrdersJson['output']:
+            jsonToSumTotalPrice = json.dumps({'DBlocation': pathToDB, 'order': order})
+            totalPriceOrderJson = json.loads(subprocess.check_output(['python', 'MSsumTotalPrice.py', jsonToSumTotalPrice], shell=True))
+            if totalPriceOrderJson['hadError']:
+                showErrorDetails(totalPriceOrderJson['error'])
+            totalIncomes += totalPriceOrderJson['output']
+        showIncomeWindow = tk.Tk()
+        showIncomeWindow.title('Incomes')
+        headerLbl = tk.Label(showIncomeWindow, text="The incomes from all the orders are", fg="green")
+        headerLbl.config(font=("Courier", 15))
+        headerLbl.grid(row=0)
+        incomeLbl = tk.Label(showIncomeWindow, text=str(totalIncomes) + '$')
+        incomeLbl.config(font=("Courier", 15))
+        incomeLbl.grid(row=1)
+
+    def showInventory(self):
+        allProductsJson = json.loads(subprocess.check_output(['python', 'getAllProductsFromDB.py', pathToDB], shell=True))
+        if allProductsJson['hadError']:
+            showErrorDetails(allProductsJson['error'])
+        subprocess.Popen(['java', '-cp', 'java-json.jar;', 'ShowAllProductsInList', json.dumps({'products': allProductsJson['output']})], shell=True)
+        print('running at background...')
+        
+
+try:
+    main = MainPane()
+    main.start()
+except Exception as err1:
+    try:
+        showErrorDetails(err1)
+        main.close()
+    except Exception as err2: #if showErrorDetails also raise exception
+        print(err2)
+

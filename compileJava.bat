c14a7b3b0fe4a39d9baf7a8a0e24319fb663c55b
Yoni Cohen <jonbir3@gmail.com>
2019-01-01 02:01:39 +0200

    Yoni creates bat file to run from CMD

---
+++ compileJava.bat
@@ -0,0 +1,6 @@
+@echo off
+javac -cp java-json.jar; ShowAllProductsInList.java
+javac -cp java-json.jar; ShowAllProductsInTable.java
+javac -cp java-json.jar; ShowError.java
+javac -cp java-json.jar; ShowOrdersDetailsInList.java
+javac -cp java-json.jar; ShowOrdersDetailsInTable.java
